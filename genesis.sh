0ef25c3325b071b9dba3aca3be91a1ffd4eec89b
Marvin <marvin@riema.xyz>
2024-04-30 16:24:40 +0800

    add nubitDa backend

---
+++ genesis.sh
@@ -0,0 +1,91 @@
+#!/bin/bash
+
+work=$(pwd)
+function generate_genesis() {
+    check_jq
+    deploy_output=${work}/contracts/docker/deploymentOutput/deploy_output.json
+    genesis=${work}/contracts/docker/deploymentOutput/genesis.json
+    create_rollup_output=${work}/contracts/docker/deploymentOutput/create_rollup_output.json
+
+#     l1Config.polygonZkEVMAddress ==> rollupAddress @ create_rollup_output.json
+#     l1Config.polygonRollupManagerAddress ==> polygonRollupManager @ deploy_output.json
+#     l1Config.polTokenAddress ==> polTokenAddress @ deploy_output.json
+#     l1Config.polygonZkEVMGlobalExitRootAddress ==> polygonZkEVMGlobalExitRootAddress @ deploy_output.json
+#     rollupCreationBlockNumber ==> createRollupBlock @ create_rollup_output.json
+#     rollupManagerCreationBlockNumber ==> deploymentBlockNumber @ deploy_output.json
+#     root ==> root @ genesis.json
+#     genesis ==> genesis @ genesis.json
+
+    rollupAddress=$(cat "$create_rollup_output" | jq -r '.rollupAddress')
+    createRollupBlock=$(cat "$create_rollup_output" | jq -r '.createRollupBlockNumber')
+
+
+    polygonRollupManager=$(cat "$deploy_output" | jq -r '.polygonRollupManagerAddress')
+    echo "polygonRollupManager: ${polygonRollupManager}"
+    polTokenAddress=$(cat "$deploy_output" | jq -r '.polTokenAddress')
+    polygonZkEVMGlobalExitRootAddress=$(cat "$deploy_output" | jq -r '.polygonZkEVMGlobalExitRootAddress')
+    deploymentBlockNumber=$(cat "$deploy_output" | jq -r '.deploymentRollupManagerBlockNumber')
+
+    root=$(cat "$genesis" | jq -r '.root')
+    genesis=$(cat "$genesis" | jq -c '.genesis')
+
+    update_genesis_json_number 'rollupCreationBlockNumber' "$createRollupBlock"
+    update_genesis_json 'l1Config.polygonZkEVMAddress' "$rollupAddress"
+
+    update_genesis_json 'l1Config.polygonRollupManagerAddress' "$polygonRollupManager"
+    update_genesis_json 'l1Config.polTokenAddress' "$polTokenAddress"
+    update_genesis_json 'l1Config.polygonZkEVMGlobalExitRootAddress' "$polygonZkEVMGlobalExitRootAddress"
+
+    update_genesis_json_number 'rollupManagerCreationBlockNumber' "$deploymentBlockNumber"
+    update_genesis_json 'root' "$root"
+    update_genesis_json_obj 'genesis' "$genesis"
+
+    sleep 3
+
+    cd ./cdk-validium-node/test/ && make run
+
+}
+
+update_genesis_json_number() {
+    genesis_cfg=${work}/cdk-validium-node/test/config/test.genesis.config.json
+    local key="$1"
+    local value="$2"
+    echo "Updating $key to $value in $genesis_cfg"
+    jq --arg val "$value" ".$key = (\$val | tonumber)" "$genesis_cfg" > temp_genesis.json && mv temp_genesis.json "$genesis_cfg"
+}
+
+update_genesis_json() {
+    genesis_cfg=${work}/cdk-validium-node/test/config/test.genesis.config.json
+    local key="$1"
+    local value="$2"
+    echo "Updating $key to $value in $genesis_cfg"
+    jq --arg val "$value" ".$key = \$val" "$genesis_cfg" > temp_genesis.json && mv temp_genesis.json "$genesis_cfg"
+}
+
+update_genesis_json_obj() {
+    genesis_cfg=${work}/cdk-validium-node/test/config/test.genesis.config.json
+    local key="$1"
+    local value="$2"
+    echo "Updating $key to $value in $genesis_cfg"
+    jq --argjson val "$value" ".$key = \$val" "$genesis_cfg" > temp_genesis.json && mv temp_genesis.json "$genesis_cfg"
+}
+
+check_jq() {
+    if ! command -v jq &> /dev/null; then
+        echo "jq is not installed. Installing now..."
+        sudo apt-get update
+
+        sudo apt-get install -y jq
+
+        if [ $? -eq 0 ]; then
+            echo "jq has been successfully installed."
+        else
+            echo "Failed to install jq. Please check your internet connection or package repository."
+            exit 1
+        fi
+    else
+        echo "jq is already installed."
+    fi
+}
+
+generate_genesis
---
+++ run.sh
@@ -0,0 +1,132 @@
+#!/bin/bash
+
+work=$(pwd)
+image_tag="latest"
+
+function check_zkevmnode(){
+    cd ${work}
+    image_name="zkevm-node"
+    image_exists=$(docker image ls --format "{{.Repository}}:{{.Tag}}" | grep "^${image_name}:${image_tag}$")
+
+    if [ -n "${image_exists}" ]; then
+        echo "Docker image ${image_name}:${image_tag} exists."
+    else
+       echo "Docker image ${image_name}:${image_tag} does not exist."
+
+       git clone https://github.com/RiemaLabs/cdk-validium-node.git
+
+       cd  ./cdk-validium-node && make build-docker
+    fi
+}
+
+
+# cd ../
+
+function check_availnode(){
+    cd ${work}
+    availnode_name="availnode"
+
+    availnode_exists=$(docker image ls --format "{{.Repository}}:{{.Tag}}" | grep "^${availnode_name}:${image_tag}$")
+
+    if [ -n "${availnode_exists}" ]; then
+        echo "Docker image ${availnode_name}:${image_tag} exists."
+    else
+       echo "Docker image ${availnode_name}:${image_tag} does not exist."
+
+       git clone https://github.com/availproject/avail.git
+
+       cd ./avail && docker build -t availnode -f ./dockerfiles/avail-node.Dockerfile .
+    fi
+}
+
+
+
+function check_geth_evm(){
+    cd ${work}
+    name="hermeznetwork/geth-zkevm-contracts"
+
+    geth_exists=$(docker image ls --format "{{.Repository}}:{{.Tag}}" | grep "^${name}:${image_tag}$")
+
+    if [ -n "${geth_exists}" ]; then
+        echo "Docker image ${name}:${image_tag} exists."
+    else
+       echo "Docker image ${name}:${image_tag} does not exist."
+
+       check_node
+
+
+       cd ./contracts && npm install --save-dev hardhat && npm i && npx hardhat compile && npm run docker:contracts
+
+       sleep 2
+
+       docker image tag hermeznetwork/geth-zkevm-contracts hermeznetwork/geth-cdk-validium-contracts:20240420
+
+
+
+    fi
+}
+
+
+
+check_node() {
+    if ! command -v node &> /dev/null; then
+        echo "Node.js is not installed. Installing v20..."
+        install_node_v20
+    else
+        echo "Node.js is installed. Checking version..."
+
+        major_version=$(echo "$node_version" | cut -d '.' -f 1)
+
+        if [ "$major_version" != "v20" ]; then
+            echo "Node.js version is not v20 (current version: $node_version). Installing v20..."
+            install_node_v20
+        else
+            echo "Node.js version is v20."
+        fi
+    fi
+}
+
+check_nvm() {
+    if  ! command -v nvm &> /dev/null; then
+         echo "NVM is not installed. Installing NVM..."
+         install_nvm
+    else
+        echo "NVM is installed."
+    fi
+}
+
+install_nvm() {
+    curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.1/install.sh | bash
+
+    # wget -qO- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.1/install.sh | bash
+
+    export NVM_DIR="$HOME/.nvm"
+    [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
+    [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"
+    sleep 2
+    if command -v nvm &> /dev/null; then
+        echo "Successfully installed NVM."
+    else
+        echo "Failed to install NVM. Please check the logs and try again manually."
+        exit 1
+    fi
+}
+
+install_node_v20() {
+    check_nvm
+
+    nvm_path=$(command -v nvm)
+
+    echo "nvm_path: ${nvm_path}"
+    "${nvm_path}" install 20
+}
+
+
+function run(){
+    git clone https://github.com/RiemaLabs/zkevm-contracts.git ./contracts
+    check_zkevmnode
+    check_availnode
+    check_geth_evm
+}
+
+run
